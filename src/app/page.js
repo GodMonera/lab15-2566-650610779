f6d48ad6976a2935df95efb5b4f1981123bef6c8
godmonera <borwonpak_du@cmu.ac.th>
2023-09-04 14:46:03 +0700

    done

--- src/app/page.js
+++ src/app/page.js
@@ -44,7 +44,10 @@ const schema = z
     }),
     hasCoupon: z.boolean(),
     coupon: z.string(),
-    password: z.string(),
+    password: z
+      .string()
+      .min(6, { message: "Password must contain at least 6 characters" })
+      .max(12, { message: "Password must not exceed 12 characters" }),
     confirmPassword: z.string(),
   })
   .refine(
@@ -55,6 +58,7 @@ const schema = z
       if (!data.hasCoupon) return true;
       // if user tick "I have coupon" and fill correct code, then it's ok too
       if (data.hasCoupon && data.coupon === "CMU2023") return true;
+
       // ticking "I have coupon" but fill wrong coupon code, show error
       return false;
     },
@@ -62,6 +66,18 @@ const schema = z
     {
       message: "Invalid coupon code",
       path: ["coupon"],
+    }
+  )
+  .refine(
+    (data) => {
+      if (data.password == data.confirmPassword) return true;
+
+      return false;
+    },
+
+    {
+      message: "Password does not match",
+      path: ["confirmPassword"],
     }
   );
 
@@ -90,9 +106,14 @@ export default function Home() {
     //TIP : get value of currently filled form with variable "form.values"
 
     if (form.values.plan === "funrun") price = 500;
+    if (form.values.plan === "mini") price = 800;
+    if (form.values.plan === "half") price = 1200;
+    if (form.values.plan === "full") price = 1500;
     //check the rest plans by yourself
     //TIP : check /src/app/libs/runningPlans.js
-
+    if (form.values.hasCoupon && form.values.coupon === "CMU2023") {
+      price = (price * 70) / 100;
+    }
     //check discount here
 
     return price;
@@ -175,7 +196,11 @@ export default function Home() {
           </Stack>
         </form>
 
-        <Footer year={2023} fullName="Chayanin Suatap" studentId="650610560" />
+        <Footer
+          year={2023}
+          fullName="Borwonpak Duangjun"
+          studentId="650610779"
+        />
       </Container>
 
       <TermsAndCondsModal opened={opened} close={close} />
